f4636d930f12a4f355aafbfd8726c67e5e13fd24
tanver909 <tanveermahidi@gmial.com>
2025-09-26 15:10:39 +0600

    category added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import Banners from "@/components/layout/HomeLayout/Banners";
 import Flashsale from "@/components/layout/HomeLayout/FlashSale/Flashsale";
 import NewProduct from "@/components/layout/HomeLayout/NewProduct/NewProduct";
 import TopBrands from "@/components/layout/HomeLayout/Topbrands/TopBrands";
+import TopCategroy from "@/components/layout/HomeLayout/TopCategory/TopCategroy";
 import TradingProduct from "@/components/layout/HomeLayout/TrandingProduct/TradingProduct";
 
 
@@ -19,6 +20,7 @@ export default function Home() {
       <Banners />
      <Flashsale/>
      <TopBrands/>
+     <TopCategroy/>
      <TradingProduct/>
      <NewProduct/>
 
